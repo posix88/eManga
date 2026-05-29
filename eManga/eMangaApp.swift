c56109c24fcea1bfd179f5de9903e141e566f458
Antonino Musolino <antonino.musolino@edreamsodigeo.com>
2026-05-29 12:34:29 +0200

    Initial Commit

---
+++ eManga/eMangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  eMangaApp.swift
+//  eManga
+//
+//  Created by Antonino.Musolino on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct eMangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eMangaTests/eMangaTests.swift
@@ -0,0 +1,17 @@
+//
+//  eMangaTests.swift
+//  eMangaTests
+//
+//  Created by Antonino.Musolino on 29/05/2026.
+//
+
+import Testing
+@testable import eManga
+
+struct eMangaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
